4905fa8523422c8aaf7d54c4ff57d0615deb0e83
Chloe <chloebrown2020@u.northwestern.edu>
2019-03-01 14:11:07 -0600

    it now graphs and is a bit smoother

--- pitchdetection.py
+++ pitchdetection.py
@@ -1,8 +1,14 @@
 import pyaudio
 import aubio
-import wave
-import sys
+import time
 import numpy as np
+import matplotlib.pyplot as plt
+
+# humming/voice 800, 100, 1000
+# whistle 800, 1000, 4000
+smoothing_threshold = 800
+lowest_freq = 100
+highest_freq = 1000
 
 # PyAudio object.
 p = pyaudio.PyAudio()
@@ -15,21 +21,46 @@ stream = p.open(format=pyaudio.paFloat32,
 # Aubio's pitch detection.
 pDetection = aubio.pitch("default", 2048,
                          2048 // 2, 44100)
+
 # Set unit.
 pDetection.set_unit("Hz")
 pDetection.set_silence(-40)
 
-while True:
+# Plotting
+samplenum = 0
+fig = plt.figure()
+plotx = []
+ploty = []
+
+start = time.time()
+
+prevsteadypitch = 0.0
+prevpitch = 0.0
+
+while time.time() - start < 10:
     data = stream.read(1024)
-    samples = np.fromstring(data,
-                             dtype=aubio.float_type)
+    samples = np.fromstring(data, dtype=aubio.float_type)
     pitch = pDetection(samples)[0]
     # Compute the energy (volume) of the
     # current frame.
-    volume = np.sum(samples ** 2) / len(samples)
+    # volume = np.sum(samples ** 2) / len(samples)
     # Format the volume output so that at most
     # it has six decimal numbers.
-    volume = "{:.6f}".format(volume)
+    # volume = "{:.6f}".format(volume)
+
+    samplenum += 1
+    plotx.append(samplenum)
+    if prevsteadypitch < lowest_freq and pitch < highest_freq:
+        prevsteadypitch = pitch
+    elif abs(prevsteadypitch - pitch) < smoothing_threshold and prevsteadypitch != 0 and pitch != 0:
+        prevsteadypitch = pitch
+
+    ploty.append(prevsteadypitch)
+
+    prevpitch = pitch
 
     print(pitch)
     # print(volume)
+
+plt.plot(plotx, ploty)
+plt.show()
