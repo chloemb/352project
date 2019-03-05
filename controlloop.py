1e45a512597dde7ac080721cb60e0373e493514e
Chloe <chloebrown2020@u.northwestern.edu>
2019-03-05 13:53:13 -0600

    calibration (use a good mic)

--- controlloop.py
+++ controlloop.py
@@ -1,4 +1,4 @@
-import sys, pygame, pitchdetection
+import sys, pygame, pitchdetection, time
 from multiprocessing import Process
 
 # humming/voice approx 100, 500
@@ -6,36 +6,97 @@ from multiprocessing import Process
 low_freq = 1000
 high_freq = 3000
 
+def GetPitch():
+    # read the pitch file
+    pitchfile = open("pitch.txt", "r")
+    readpitch = pitchfile.readline()
+    pitchfile.close()
+    return readpitch
+
 
 def GameLoop():
     pygame.init()
 
-    size = width, height = 320, 800
+    size = width, height = 800, 800
     black = 0, 0, 0
+    white = 255, 255, 255
 
     screen = pygame.display.set_mode(size)
 
+    pygame.font.init()
+    myfont = pygame.font.Font("Assets/JosefinSans-Regular.ttf", 30)
+
+    checkpoint_time = time.time()
+    curpitch = 0
+
+    while time.time() - checkpoint_time < 5:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: sys.exit()
+
+        screen.fill(black)
+
+        textsurface = myfont.render('Sing or whistle a low note', False, white)
+        screen.blit(textsurface, (width/2 - textsurface.get_width()/2, height/2 - textsurface.get_height()/2))
+
+        readpitch = GetPitch()
+        if readpitch is not "":
+            curpitch = float(readpitch)
+
+        global low_freq
+        low_freq = curpitch
+
+        frequency = myfont.render(str(curpitch), False, white)
+        screen.blit(frequency, (width/2 - frequency.get_width()/2,
+                                height/2 - frequency.get_height()/2 + textsurface.get_height()))
+        pygame.display.flip()
+
+    checkpoint_time = time.time()
+
+    while time.time() - checkpoint_time < 5:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: sys.exit()
+
+        screen.fill(black)
+
+        textsurface = myfont.render('Sing or whistle a high note', False, white)
+        screen.blit(textsurface, (width / 2 - textsurface.get_width() / 2, height / 2 - textsurface.get_height() / 2))
+
+        readpitch = GetPitch()
+        if readpitch is not "":
+            curpitch = float(readpitch)
+
+        global high_freq
+        high_freq = curpitch
+
+        frequency = myfont.render(str(curpitch), False, white)
+        screen.blit(frequency, (width / 2 - frequency.get_width() / 2,
+                                height / 2 - frequency.get_height() / 2 + textsurface.get_height()))
+        pygame.display.flip()
+
+    print(low_freq, high_freq)
     ball = pygame.image.load("intro_ball.gif")
     ballrect = ball.get_rect()
-
-    curpitch = 0
+    curheight = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT: sys.exit()
 
-        # read the pitch file
-        pitchfile = open("pitch.txt", "r")
-        readpitch = pitchfile.readline()
-        pitchfile.close()
+        readpitch = GetPitch()
         if readpitch is not "":
             curpitch = float(readpitch)
 
-        # calculate the height
+        if curpitch < low_freq:
+            curpitch = low_freq
+        if curpitch > high_freq:
+            curpitch = high_freq
+
         curheight = height - int((curpitch-low_freq)/(high_freq-low_freq) * height)
 
+        print(curpitch, curheight)
+
         # move the ball
-        ballrect.center = (160, curheight)
+        ballrect.center = (width/2, curheight)
 
         # draw
         screen.fill(black)
@@ -46,7 +107,7 @@ def GameLoop():
 if __name__ == '__main__':
     # pitchdetection.initpitch()
 
-    a = Process(target=pitchdetection.pitchdetection, args=(high_freq, low_freq))
+    a = Process(target=pitchdetection.pitchdetection, args=(50, 5000))
     a.start()
     b = Process(target=GameLoop)
     b.start()
--- pitchdetection.py
+++ pitchdetection.py
@@ -4,13 +4,9 @@ import time
 import numpy as np
 
 
-# def initpitch():
-#     global currentpitch
-#     currentpitch = 0
-
-
-def pitchdetection(highest_freq, lowest_freq):
-    smoothing_threshold = (highest_freq - lowest_freq) / 2
+def pitchdetection(lowest_freq, highest_freq):
+    print(lowest_freq, highest_freq)
+    # smoothing_threshold = (highest_freq - lowest_freq) / 2
 
     # PyAudio object.
     p = pyaudio.PyAudio()
@@ -28,30 +24,31 @@ def pitchdetection(highest_freq, lowest_freq):
     pDetection.set_unit("Hz")
     pDetection.set_silence(-40)
 
-    # Plotting
-    # samplenum = 0
-    # fig = plt.figure()
-    # plotx = []
-    # ploty = []
-
     prevsteadypitch = 0.0
 
     while True:
         data = stream.read(1024)
         samples = np.fromstring(data, dtype=aubio.float_type)
         pitch = pDetection(samples)[0]
+        # Compute the energy (volume) of the
+        # current frame.
+        volume = np.sum(samples ** 2) / len(samples)
+        # Format the volume output so that at most
+        # it has six decimal numbers.
+        volume = "{:.6f}".format(volume)
 
-        if not pitch < lowest_freq and not pitch > highest_freq:
-            if prevsteadypitch < lowest_freq and pitch < highest_freq:
+        if float(volume) > .001:
+            if not pitch < lowest_freq and not pitch > highest_freq:
+                # if prevsteadypitch < lowest_freq and pitch < highest_freq:
+                #     prevsteadypitch = pitch
+                # # elif abs(prevsteadypitch - pitch) < smoothing_threshold and prevsteadypitch != 0 and pitch != 0:
+                # #     prevsteadypitch = pitch
+                # else:
+                #     prevsteadypitch = pitch
                 prevsteadypitch = pitch
-            elif abs(prevsteadypitch - pitch) < smoothing_threshold and prevsteadypitch != 0 and pitch != 0:
-                prevsteadypitch = pitch
+
+        # print(volume)
 
         pitchfile = open("pitch.txt", "w+")
         pitchfile.write(str(prevsteadypitch))
         pitchfile.close()
-#
-#
-# def getcurpitch():
-#     global currentpitch
-#     return currentpitch
